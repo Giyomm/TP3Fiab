6ad780c5f9162110ef90d4c429668f2c3d3e8f68
RIVAS Guillaume <r21101999@tempo21-2.luminy.univ-amu.fr>
2016-10-10 10:24:17 +0200

    basic jdbc methods

--- src/main/InteractBD.java
+++ src/main/InteractBD.java
@@ -1,9 +1,28 @@
 package main;
 
-public class InteractBD {
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-	public InteractBD() {
-		// TODO Auto-generated constructor stub
+import com.mysql.jdbc.Connection;
+
+public class InteractBD {
+	
+	private final String LOGIN = "r21101999";
+	private final String PASSWD = "5Gf;wh";
+
+	public InteractBD() {}
+	
+	public Connection connect(String url) throws SQLException{
+		Connection conn = (Connection) DriverManager.getConnection(url, LOGIN, PASSWD);
+		return conn;
+	}
+	
+	public void deconnect(Connection conn) throws SQLException{
+		conn.close();
+	}
+	
+	public boolean isConnected(Connection conn) throws SQLException{
+		return ! conn.isClosed();
 	}
 
 }
